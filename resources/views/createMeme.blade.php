85579ccfc569f930a95c1b5baa6185039c84555f
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2024-08-29 09:16:59 +0000

    Aug 29, 2024, 12:16 PM

--- resources/views/createMeme.blade.php
+++ resources/views/createMeme.blade.php
@@ -65,15 +65,19 @@
             <form class=" text-lg  text-center text-[#374151]">
                 <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-                        State
+                        MemeType
                     </label>
                     <div class="relative">
                         <select
                             class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                             id="grid-state">
-                            <option>New Mexico</option>
-                            <option>Missouri</option>
-                            <option>Texas</option>
+                            <option value="0"></option>
+                            <option value="1"></option>
+                            <option value="2"></option>
+                            <option value="3"></option>
+                            <option value="4"></option>
+                            <option value="5"></option>
+                            <option value="6"></option>
                         </select>
                         <div
                             class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
