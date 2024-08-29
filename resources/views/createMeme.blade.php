6b05597b294be2aa612402965646fab5f7c76355
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2024-08-29 09:20:20 +0000

    Aug 29, 2024, 12:20 PM

--- resources/views/createMeme.blade.php
+++ resources/views/createMeme.blade.php
@@ -69,7 +69,7 @@
                     </label>
                     <div class="relative">
                         <select
-                            class="block appearance-none w-100 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            class="block appearance-none w-auto bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                             id="grid-state">
                             <option value="0">Image With URL</option>
                             <option value="1">Video With URL</option>
@@ -80,7 +80,7 @@
                             <option value="6">Gif With Upload</option>
                         </select>
                         <div
-                            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            class="pointer-events-none absolute inset-y-0 left-0 flex items-center px-2 text-gray-700">
                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
                             </svg>
