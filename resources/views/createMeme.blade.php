966343a69845194f8232cda317268729284286b2
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2024-08-29 09:32:13 +0000

    Aug 29, 2024, 12:32 PM

--- resources/views/createMeme.blade.php
+++ resources/views/createMeme.blade.php
@@ -109,7 +109,7 @@
                     <input
                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                         id="inline-full-name" name="title" type="text">
-                    <input type="hidden" name="memeType" value="{{ $request->get("memeType")->default(0) }}">
+                    <input type="hidden" name="memeType" value="{{ $request->get("memeType",0) }}">
                     </div>
             </div>
             <div class="md:flex md:items-center mb-6">
