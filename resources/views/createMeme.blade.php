854c3a043738c12972e44b0fd7804787d0a20ac7
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2024-08-29 09:28:52 +0000

    Aug 29, 2024, 12:28 PM

--- resources/views/createMeme.blade.php
+++ resources/views/createMeme.blade.php
@@ -102,7 +102,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-                        Meme Title
+                        Meme Title ({{ $request->get("memeType") }})
                     </label>
                 </div>
                 <div class="md:w-2/3">
