2d1ca8f0a73f63902b178948389896195d9212b1
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2024-09-01 22:20:22 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,11 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-<<<<<<< HEAD
 use App\Http\Middleware\CheckJWT;
-=======
-
->>>>>>> b98b541232b7309f7089d928c0f8710976c5aafa
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -15,13 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-<<<<<<< HEAD
         $middleware->appendToGroup('checkSession', [
             CheckJWT::class,
         ]);
-=======
-        //
->>>>>>> b98b541232b7309f7089d928c0f8710976c5aafa
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
